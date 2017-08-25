e818e8e154780f8c4c917f107955f2aa1e0397d1
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-25 16:12:13 -0700

    create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+//Run jquery
+$(document).ready(function(){
+ 
+ var url= "https://api.twitch.tv/kraken/stream/freecodecamp";
+
+ $.getJSON(url,function(data1){
+ if (data1.stream===null){
+  $("#fccStatus").html("Free Code Camp is currently offline")
+ }
+  else(
+  $("#fccStatus").html("Free Code Camp is currently online")
+   )
+ }
+          
+});
+});
+
+
+$(document).ready(function(){
+ //free code camp streaam info and status API call
+ var url= "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
+ //free code camp stream info and status API call
+
+ $.getJSON(url,function(data1){
+ if (data1.stream===null){
+  $("#fccStatus").html("Free Code Camp is currently offline");
+ }
+  else{
+  $("#fccStatus").html("Free Code Camp is currently online");
+  }
+ });
+
+
+   });
