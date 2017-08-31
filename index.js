022dc30922c2c2bd93778b99edc8e5ec736ab4ec
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-31 11:47:22 -0700

    updated API

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Run our JQUERY
 $(document).ready(function(){
- var following=[];
  //Freecodecamp Stream info status API call
  var url= "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
  $.getJSON(url,function(data1){
@@ -13,15 +12,15 @@ $(document).ready(function(){
   }
      
  });
+ 
+var followerURL= "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels/";
+$.getJSON(followerURL, function(data2){
+ for (var i=0;i<data2.follows.length;i++){
+ var displayName= data2.follows[i].channel.display_name;
+console.log(displayName);
+ }
+ });
+ });
 
- var followerURL= "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp/follows";  
- $.getJSON(followerURL, function(data2){
-for (var i=0;i<data2._links.follows.length;i++){
-var displayName= data2.follows[i].channel.display_name;
+
  
- following.push(displayName);
- 
-}
-console.log(following);
-});
-});
