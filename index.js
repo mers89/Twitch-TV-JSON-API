f879bf948012500762df50209f674310b9181640
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-31 18:56:03 -0700

    getJSON

--- index.js
+++ index.js
@@ -29,13 +29,22 @@ following.push("ESL_SC2");
     for(var i=0; i <following.length; i++){
     var url2 = 'https://wind-bow.glitch.me/twitch-api/channels/%22+following[i]+%22?callback=?';
 
-$.getJSON(url2).done(function(data3){
+ $.getJSON(url2, function(data3){
         var logo;
         var status;
         var name;
  if (data3.error){
-  console.log(url2);
+ logo= "http://media.istockphoto.com/vectors/error-404-vector-id538038858";
+   name = data3.message;
+   status= data3.error;
+  
+   $("#followerInfo").prepend("div class = 'row'>" | "<div class='col-md-4'>" | "img src='" + logo + "'>"
+    +
+   "</div>" + "<div class='col-md-4'>"  + name + "</div>" + "<div class='col-md-4'>" + status + "</div> </div>");       
+
+ 
   }
+  
 
  });
 
@@ -51,5 +60,7 @@ $.getJSON(url2).done(function(data3){
  }
  
  );
+
+
 
 
