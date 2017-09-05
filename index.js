2a4752f2eb7abf618f80dd75ae4d6d742dd3f929
Mercy Manrique <mercymanrique@yahoo.com>
2017-09-04 19:33:25 -0700

    .prepend

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
        $("#followerinfo")
           .prepend("<img src='"+ data3.logo + "'>")
           .append("<div>" + data3.name+ "</div>")
-          .append("<div>" + data3.status + "</div>");
+          .prepend("<div>" + data3.status + "</div>");
       } else {
         var logo= "http://res.cloudinary.com/mers/image/upload/c_scale,w_400/v1504573315/ui-day033-404error_u37kee.png";
          $("#followerinfo")
