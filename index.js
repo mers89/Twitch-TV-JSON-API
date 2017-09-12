a868b583e893be0c592de82cd048196e044840dc
Mercy Manrique <mercymanrique@yahoo.com>
2017-09-12 15:37:03 -0700

    update index.js

--- index.js
+++ index.js
@@ -28,22 +28,30 @@ $(document).ready(function(){
   
       
      $.getJSON(url2).done(function(data3) {
+      
+      console.log("the returned data3 is ", data3);
+      var errorLogo= "http://res.cloudinary.com/mers/image/upload/c_scale,w_175/v1504573315/ui-day033-404error_u37kee.png";
+      
     if (data3.error == undefined) {
+    var picUrl = data3.logo != null ? data3.logo : errorLogo;
     $("#logo")
-      .append("<img class='logo_row' src='" + data3.logo + "'>");
-    // var logoHeight = 200
+      .append("<img class='logo_row' src='" + picUrl  + "'>");
+    var logoHeight = 200
      $("#displayname")
       .append('<div class="status_row">' + data3.display_name + "</div>");
 
      $("#status")
       .append('<div class="status_row">'  + data3.status + "</div>");
-      } else {
+      } 
+      
+ /*     
+      else {
         var logo= "http://res.cloudinary.com/mers/image/upload/c_scale,w_175/v1504573315/ui-day033-404error_u37kee.png";
          $("#logo")
           .append("<img class='logo_row' src='"+ logo + "'>") ;
      }});
      
-
+*/
  
   //var parsedata = function(data3) {
   // console.log(data3)
